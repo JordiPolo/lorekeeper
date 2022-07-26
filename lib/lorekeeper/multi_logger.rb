9b2df1b585549e09b16c26e791451e35b79cca33
Minira Samadova <samadova.minira@gmail.com>
2022-07-26 16:30:22 +0900

    get rid of metaprogramming once and for all; explicitly define logging methods

--- lib/lorekeeper/multi_logger.rb
+++ lib/lorekeeper/multi_logger.rb
@@ -17,15 +17,20 @@ module Lorekeeper
     end
 
     # Define all common logging methods within Multilogger to avoid NoMethodError
-    FastLogger::LOGGING_METHODS.each do |method_name|
-      define_method method_name.to_s, ->(*args, &block) do
-        call_loggers(method_name, *args, &block)
-      end
+    def debug(*args, &block); call_loggers(:debug, *args, &block); end
+    def debug_with_data(*args, &block); call_loggers(:debug, *args, &block); end
 
-      define_method "#{method_name}_with_data", ->(*args, &block) do
-        call_loggers(method_name, *args, &block)
-      end
-    end
+    def info(*args, &block); call_loggers(:info, *args, &block); end
+    def info_with_data(*args, &block); call_loggers(:info, *args, &block); end
+
+    def warn(*args, &block); call_loggers(:warn, *args, &block); end
+    def warn_with_data(*args, &block); call_loggers(:warn, *args, &block); end
+
+    def error(*args, &block); call_loggers(:error, *args, &block); end
+    def error_with_data(*args, &block); call_loggers(:error, *args, &block); end
+
+    def fatal(*args, &block); call_loggers(:fatal, *args, &block); end
+    def fatal_with_data(*args, &block); call_loggers(:fatal, *args, &block); end
 
 if RUBY_VERSION > "2.6"
     def respond_to?(method, all_included = false)
