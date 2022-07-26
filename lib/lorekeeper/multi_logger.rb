ec612dc2f0357cd42fc87d5e7285cebd7d76f574
Minira Samadova <samadova.minira@gmail.com>
2022-07-26 16:50:13 +0900

    rubocop fix

--- lib/lorekeeper/multi_logger.rb
+++ lib/lorekeeper/multi_logger.rb
@@ -18,29 +18,34 @@ module Lorekeeper
 
     # Define all common logging methods within Multilogger to avoid NoMethodError
     def debug(*args, &block); call_loggers(:debug, *args, &block); end
+
     def debug_with_data(*args, &block); call_loggers(:debug, *args, &block); end
 
     def info(*args, &block); call_loggers(:info, *args, &block); end
+
     def info_with_data(*args, &block); call_loggers(:info, *args, &block); end
 
     def warn(*args, &block); call_loggers(:warn, *args, &block); end
+
     def warn_with_data(*args, &block); call_loggers(:warn, *args, &block); end
 
     def error(*args, &block); call_loggers(:error, *args, &block); end
+
     def error_with_data(*args, &block); call_loggers(:error, *args, &block); end
 
     def fatal(*args, &block); call_loggers(:fatal, *args, &block); end
+
     def fatal_with_data(*args, &block); call_loggers(:fatal, *args, &block); end
 
-if RUBY_VERSION > "2.6"
-    def respond_to?(method, all_included = false)
-      @loggers.all? { |logger| logger.respond_to?(method, all_included) }
+    if RUBY_VERSION > '2.6'
+      def respond_to?(method, all_included: false)
+        @loggers.all? { |logger| logger.respond_to?(method, all_included) }
+      end
+    else
+      def respond_to?(method)
+        @loggers.all? { |logger| logger.respond_to?(method) }
+      end
     end
-else
-    def respond_to?(method)
-      @loggers.all? { |logger| logger.respond_to?(method) }
-    end
-end
 
     def call_loggers(method, *args, &block)
       result = @loggers.map do |logger|
